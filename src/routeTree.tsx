475fc30e747c77d6c3aea90d543393eb6c091746
km-v1 <308789073+kristinavirtualassistant-coder@users.noreply.github.com>
2026-09-18 02:26:48 +0800

    feat: route Better Auth signup page

--- src/routeTree.tsx
+++ src/routeTree.tsx
@@ -6,9 +6,10 @@ import { OwnersPage } from './routes/owners'
 import { ImportsPage } from './routes/imports'
 import { Route as authRoute } from './routes/api/auth/$'
 import { Route as loginRoute } from './routes/login'
+import { Route as signupRoute } from './routes/signup'
 const rootRoute=createRootRoute({component:RootLayout})
 const dashboardRoute=createRoute({getParentRoute:()=>rootRoute,path:'/',component:DashboardPage})
 const propertiesRoute=createRoute({getParentRoute:()=>rootRoute,path:'/properties',component:PropertiesPage})
 const ownersRoute=createRoute({getParentRoute:()=>rootRoute,path:'/owners',component:OwnersPage})
 const importsRoute=createRoute({getParentRoute:()=>rootRoute,path:'/imports',component:ImportsPage})
-export const routeTree=rootRoute.addChildren([dashboardRoute,propertiesRoute,ownersRoute,importsRoute,loginRoute,authRoute])
+export const routeTree=rootRoute.addChildren([dashboardRoute,propertiesRoute,ownersRoute,importsRoute,loginRoute,signupRoute,authRoute])
